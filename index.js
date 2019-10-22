df45852d1b830328d81f61e81b481f3a4e57f94f
samliebl <stliebl@gmail.com>
2019-10-22 15:41:14 -0400

    removed conditional checking the parameter type.

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 function regexMapReplace(str, map) {
-	if (typeof(str) != String) {
-		throw new Error('Recieved non-string data type for first parameter/str. The first parameter/str needs to be a string.');
-	} else if (typeof(map) != Array) {
-		throw new Error('Recieved non-string data type for second parameter/map. The second parameter/map needs to be an array.');
-	}
     map.forEach(function(err, regexItem) {
         str = str.replace(map[regexItem].match, map[regexItem].replace);
-	});
-  	return str;
+    });
+    return str;
 }
 
 module.exports = regexMapReplace;
